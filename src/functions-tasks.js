2a890bedd8ad27923ab4d475e21640433f10bb67
anakdrch <ana.kudrevich@gmail.com>
2025-04-20 22:25:08 +0200

    feat: implement getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -195,8 +195,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  const generator = () => {
+    const res = counter;
+    counter += 1;
+    return res;
+  };
+  return generator;
 }
 
 module.exports = {
